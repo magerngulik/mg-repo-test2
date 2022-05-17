cc6b50ae776f4a6ece57f50e3c6ea560dcbf164f
magerngulik <magerngulik@gmail.com>
2022-05-17 22:29:02 +0700

    menambahkan data index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, aspernatur. Ipsum, tenetur blanditiis. Expedita cupiditate ratione ex quasi laboriosam vero, dolore iusto doloremque culpa, molestiae architecto dolorem amet ipsam aut.</p>
+</body>
+</html>
